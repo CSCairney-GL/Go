0823d5dd63f1bc1fc94e7e0827c42b25ec12423c
CSCairney-GL <119429189+CSCairney-GL@users.noreply.github.com>
2023-05-16 11:03:16 +0100

    Initial Structure built for Go

---
+++ game-go/src/components/player/Player.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Player = () => {
+        return (
+            <div>
+                
+            </div>
+        );
+}
+
+export default Player;
